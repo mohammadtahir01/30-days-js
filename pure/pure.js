71a2171f7b3ce6a78aa3ddcf7f3114e43a7d0b9b
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2025-04-28 23:31:38 +0530

    update

---
+++ pure/pure.js
@@ -0,0 +1,39 @@
+
+
+// let Btn = document.querySelector("#btn");
+
+// Btn.addEventListener("click",()=>{
+//     alert("helo")
+// })
+
+
+// let Inp =document.querySelector('#inp');
+
+// Inp.addEventListener('keydown',(e)=>{
+// console.log(e.key)
+// })
+
+// Inp.addEventListener('input',()=>{
+//     console.log(Inp.value)
+//     })
+
+
+    // let Inp =document.querySelector('#inp');
+    // let inp2 = document.querySelector('#show');
+
+    // Inp.addEventListener('keydown',(e)=>{
+    // console.log(e.key)
+    // })
+    
+    // Inp.addEventListener('input',()=>{
+    //    inp2.innerHTML=`<h1>${Inp.value}</h1>`;
+    // })  
+
+    
+//   to-do list  
+    
+let head =document.createElement('h1');  
+head.innerText="helo tahir";
+
+let Box = document.querySelector("#h2");
+Box.appendChild(head)
